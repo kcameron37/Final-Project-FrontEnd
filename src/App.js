5054fc70bcc99edcf5e6656517123d142ee15f43
kcameron37 <115664906+kcameron37@users.noreply.github.com>
2023-11-07 16:09:06 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>This is our super fancy job board </h1>
+      <h2>Corey, Giovana, Kaitlyn & Sara </h2>
+    </div>
+  );
+}
