5c3c4480ac9bb053878ef8f99ba271559874955a
kcameron37 <115664906+kcameron37@users.noreply.github.com>
2023-11-28 15:50:35 -0800

    featured jobs

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -44,6 +44,53 @@ export default function JobList({ jobs, deleteJob }) {
           <p className={styles.jobDescription}>{job.description}</p>
         </div>
       ))}
+  <div className={styles.jobListContainer}>
+        <div className={styles.jobItem}>
+          <div className={styles.titleLine}>
+            <h3 className={styles.jobTitle}>Software Developer</h3>
+
+            <div className={styles.companyInfo}>
+              <div className={styles.companyName}>
+                <span>Tech Solutions Inc.</span>
+              </div>
+            </div>
+            <div className={styles.companyDetails}>
+              <span>Full Time</span>
+              <span>Vancouver, BC</span>
+              <span>$100,000</span>
+            </div>
+          </div>
+          <p className={styles.jobDescription}>
+            We are seeking a talented software developer to join our dynamic
+            team. The ideal candidate will have strong programming skills and a
+            passion for creating innovative software solutions.
+          </p>
+        </div>
+      </div>
+
+      <div className={styles.jobListContainer}>
+        <div className={styles.jobItem}>
+          <div className={styles.titleLine}>
+            <h3 className={styles.jobTitle}>Marketing Specialist</h3>
+            <div className={styles.companyInfo}>
+              <div className={styles.companyName}>
+                <span>Digital Marketing Agency</span>
+              </div>
+            </div>
+            <div className={styles.companyDetails}>
+              <span>Full Time</span>
+              <span>Vancouver, BC</span>
+              <span>$200,000</span>
+            </div>
+          </div>
+          <p className={styles.jobDescription}>
+            Join our marketing team to develop and implement creative marketing
+            strategies. The ideal candidate will have a strong understanding of
+            digital marketing trends and excellent communication skills
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
+
